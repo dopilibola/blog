f4986058e47c86e7ee1a16fe5951b4ab0154d6b1
Your Name <your.email@example.com>
2024-11-26 15:46:42 +0500

    3

--- postscribers/blog/views.py
+++ postscribers/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Index Page')
+    return render(request, 'blog/index.html')
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'about')

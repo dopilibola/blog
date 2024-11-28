5199295cd96bc40ad45ea077f981d69a50a952c4
Your Name <your.email@example.com>
2024-11-28 09:06:45 +0500

    sqllite -> postsql

--- postscribers/postscribers/settings.py
+++ postscribers/postscribers/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'postscribers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'databasename',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 

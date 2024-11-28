f1adf5b58fffe839e7f2093f0b0b767af9b58eb8
Your Name <your.email@example.com>
2024-11-28 10:59:15 +0500

    6

--- postscribers/postscribers/settings.py
+++ postscribers/postscribers/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
